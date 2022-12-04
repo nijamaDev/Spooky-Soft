fd9ea4478dafd48da87ca343e917671443865010
Alejandro Caicedo <alejandrocaicedo@MacBook-Pro-de-Alejandro.local>
2022-12-04 13:32:51 -0500

    Login normal por url

--- back/back/basic/urls.py
+++ back/back/basic/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('', views.index),
     path('getUsers/', views.getUsers),
     path('getStatus/', views.getStatus),
-    path('createPerson/<str:name>/<str:lastname>/<str:identification>', views.createPerson)
+    path('createPerson/<str:name>/<str:lastname>/<str:identification>', views.createPerson),
+    path('login/<str:email>/<str:password>', views.logIn),
+    path('login/google/<str:email>', views.logInGoogle)
+
 ]
--- back/back/basic/views.py
+++ back/back/basic/views.py
@@ -19,4 +19,27 @@ def getStatus(req):
 
 def createPerson(req, name, lastname, identification):
     cp = People.objects.create(name=name, lastname=lastname, identification=identification)
+    #print(req.POST['name'])
     return JsonResponse({'insercion': cp})
+
+
+def logIn(req, email, password):
+    res = {'status': 0, 'user':{}, 'msg':""}
+    user =list(Users.objects.filter(mail=email).values())
+    if len(user) > 0:
+        if user[0]['password'] == password:
+            res['status'] = 1
+            res['user'] = user[0]
+            res['msg'] = "Login Exitoso"
+            return JsonResponse(res)
+        else:
+            res['msg'] = "Contraseña Incorecta"
+            return JsonResponse(res)
+    else:
+        res['msg'] = "Usuario no Registrado"
+        return JsonResponse(res)
+
+def logInGoogle(req, emal):
+    res = {'status': 0, 'user':{}, 'msg':""}
+    #Buscar en tabla de gooale
+    return JsonResponse(res)
