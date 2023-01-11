6104592ef87a309937e640afa11097db13a91821
AlejoCx2 <alejandro.caicedo.caicedo@correounivalle.edu.co>
2023-01-11 15:30:47 -0500

    de vuelta al inicio selenuim

--- back/basic/scraping.py
+++ back/basic/scraping.py
@@ -3,27 +3,23 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
-#from selenium.webdriver.chrome.service import Service
-#from chromedriver_py import binary_path # this will get you the path variable
-#from selenium_firefox import Firefox
-#from selenium.webdriver.firefox.options import Options
 
 def scrapElement():
     # Establecer parametros
-    #browser = Firefox()
-
-    #options = webdriver.ChromeOptions()
-    #options.add_argument('--ignore-certificate-errors')
-    #options.add_argument('--incognito')
-    #options.add_argument('--headless')
+    options = webdriver.ChromeOptions()
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--incognito')
+    options.add_argument('--headless')
+    '''
     options = webdriver.FirefoxOptions()
     options.add_argument('-headless')
     browser = webdriver.Firefox(executable_path='/vercel/path1/geckodriver', options=options, log_path='/dev/null', service_log_path='/dev/null')
+    '''
     # Crear una instancia del navegador
     #service_object = Service(binary_path)
     #browser = webdriver.Chrome(service=service_object,chrome_options=options)
     #browser = webdriver.Chrome(executable_path='chromedriver',chrome_options=options)
-    #browser = webdriver.Chrome(chrome_options=options)
+    browser = webdriver.Chrome(chrome_options=options)
 
     # Usar el método get para abrir una página web
     browser.get('https://www.falabella.com.co/')
