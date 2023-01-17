1ca648ad6df3a68fe62036f3a65611d5e2dea094
AlejoCx2 <alejandro.caicedo.caicedo@correounivalle.edu.co>
2023-01-16 21:52:29 -0500

    scraping Falabella

--- back/basic/scraping.py
+++ back/basic/scraping.py
@@ -5,9 +5,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
-#import time
 
-def scrapElement():
+def descuentos(tipo,prompt):
     # Establecer parametros
     options = webdriver.ChromeOptions()
     options.add_argument('--ignore-certificate-errors')
@@ -18,48 +17,74 @@ def scrapElement():
     browser = webdriver.Chrome(chrome_options=options)
 
     # Usar el método get para abrir una página web
-    browser.get('https://www.falabella.com.co/falabella-co/collection/calzado?facetSelected=true&f.product.attribute.G%C3%A9nero=Hombre&f.range.derived.variant.discount=20%25+dcto+y+m%C3%A1s')
+    if tipo == 'Hombre':
+        browser.get('https://www.falabella.com.co/falabella-co/collection/calzado?facetSelected=true&f.product.attribute.G%C3%A9nero=Hombre&f.range.derived.variant.discount=20%25+dcto+y+m%C3%A1s')
+    elif tipo == 'Mujer':
+        browser.get('https://www.falabella.com.co/falabella-co/collection/calzado?&facetSelected=true&f.product.attribute.G%C3%A9nero=Mujer&f.range.derived.variant.discount=20%25+dcto+y+m%C3%A1s')
+    elif tipo == 'Niños':
+        browser.get('https://www.falabella.com.co/falabella-co/collection/calzado?facetSelected=true&f.product.attribute.G%C3%A9nero=Ni%C3%B1o&f.range.derived.variant.discount=20%25+dcto+y+m%C3%A1s')
+    elif tipo == 'Niñas':
+        browser.get('https://www.falabella.com.co/falabella-co/collection/calzado?facetSelected=true&f.product.attribute.G%C3%A9nero=Ni%C3%B1a&f.range.derived.variant.discount=20%25+dcto+y+m%C3%A1s')
+    elif tipo == 'Personalizada':
+        browser.get('https://www.falabella.com.co/falabella-co')
+        SearchInput = browser.find_element(By.ID, "testId-SearchBar-Input")
+        SearchInput.send_keys(prompt + Keys.ENTER)
+    
 
-    print(browser.get_window_size())
     # Espera a que el elemento este listo
     WebDriverWait(browser, timeout=4).until(EC.presence_of_element_located((By.ID, "testId-searchResults-products")))
 
     # Scroll down para cargar los productos
     total_height = int(browser.execute_script("return document.body.scrollHeight"))
-    for i in range(1, total_height, 40):
+    for i in range(1, total_height, 30):
         browser.execute_script("window.scrollTo(0, {});".format(i))
         #time.sleep(0.1)
     #browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-    #SearchInput = browser.find_element(By.ID, "testId-SearchBar-Input")
-    #SearchInput.send_keys("Botas" + Keys.ENTER)
     #browser.implicitly_wait(10)
     page_sourse = browser.page_source
-    #print(page_sourse)
-    
-    #e = link_element.text
-    # Hacer clic en el elemento
-    # link_element.click()
 
     # Scrapping
     soup = BeautifulSoup(page_sourse, 'html.parser')
     serchResults = soup.find(id='testId-searchResults-products')
     zapatos = serchResults.find_all('div', attrs={"data-pod": "catalyst-pod"})
     res = []
-    aux = 1
-    for zapato in zapatos:
-        item = {
-            'dataKey': zapato['data-key'],
-            'url_product': zapato.a['href'],
-            'url_picture': zapato.div.div.a.img['src'],    
-        }
-        print(zapato.div.next_sibling.next_sibling)
-            
-        print(aux)
-        aux += 1
-        #print(zapato['data-key'])
 
+    for zapato in zapatos:
+        marca = zapato.div.next_sibling.a.div.get_text()
+        nombre = zapato.div.next_sibling.a.div.next_sibling.get_text()
+        idn = zapato.div.next_sibling.a.div.next_sibling.next_sibling.get_text()
+        colorStr = ""
+        precios = [" ", " "]
+        aux=0
+        # Buscar Colores
+        if zapato.div.next_sibling.ul != None:
+            for c in zapato.div.next_sibling.ul.find_all('li'):
+                colorStr += c.button['style']
+        # Buscar Precios
+        for p in zapato.div.next_sibling.next_sibling.ol.find_all('li'):
+            precios[aux] = p.get_text()
+            aux += 1
+        price = precios[0].split(" ")[2]
+        if precios[1] != " ":
+            priceSale = precios[1].split(" ")[2]
+        else:
+            priceSale = " "
+        
+        item = {
+            'store': 'Falabella',
+            'name': nombre,
+            'description': f'{marca} - {nombre} \nDisponible ahora. {idn}',
+            'price': price if price != "-" else precios[0].split(" ")[1],
+            'priceSale': priceSale,
+            'location': zapato['data-key'],
+            'redirect': zapato.a['href'],
+            'cover': zapato.div.div.a.img['src'],
+            'colors': colorStr,
+        }
+        res.append(item)
+        
     # Salir
     browser.quit()
     
-    return " "
+    return res
