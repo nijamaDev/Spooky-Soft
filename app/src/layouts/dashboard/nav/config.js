83eca2834b3f30d75963213ac6ecabba293fe491
nijama <9n0101@gmail.com>
2023-01-19 20:38:59 -0500

    version 1.0

--- app/src/layouts/dashboard/nav/config.js
+++ app/src/layouts/dashboard/nav/config.js
@@ -3,33 +3,33 @@ import SvgColor from '../../../components/svg-color';
 
 // ----------------------------------------------------------------------
 
-const icon = (name) => <SvgColor src={`/assets/icons/navbar/${name}.svg`} sx={{ width: 1, height: 1 }} />;
+const icon = (name) => <SvgColor src={`/assets/icons/navbar/${name}`} sx={{ width: 1, height: 1 }} />;
 
 const navConfig = [
   {
     title: 'dashboard',
     path: '/dashboard/app',
-    icon: icon('ic_analytics'),
+    icon: icon('ic_analytics.svg'),
   },
   {
     title: 'users',
     path: '/dashboard/user',
-    icon: icon('ic_user'),
+    icon: icon('ic_user.svg'),
   },
   {
     title: 'blog',
     path: '/dashboard/blog',
-    icon: icon('ic_blog'),
+    icon: icon('ic_blog.svg'),
   },
   {
     title: 'products',
     path: '/dashboard/products',
-    icon: icon('ic_cart'),
+    icon: icon('ic_cart.svg'),
   },
   {
     title: 'scrapping',
     path: '/dashboard/scrapping',
-    icon: icon('GiWarPick'),
+    icon: icon('pickaxe.png'),
   },
 ];
 
